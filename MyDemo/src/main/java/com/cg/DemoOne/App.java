b9a7b24449b03612edf987ac5365315cfb588847
Your Name <you@example.com>
2017-07-12 21:45:23 +0530

    Commit Mydemo First

---
+++ MyDemo/src/main/java/com/cg/DemoOne/App.java
@@ -0,0 +1,15 @@
+package com.cg.DemoOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       B b=new B();
+       b.getData();
+    }
+}
---
+++ MyDemo/src/main/java/com/cg/DemoOne/B.java
@@ -0,0 +1,6 @@
+package com.cg.DemoOne;
+class B{
+public void getData(){
+System.out.println("WELCOME TO DEVOPS");
+}
+}
